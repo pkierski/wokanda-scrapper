96e33421c6b9f061a333363a01c6841fd1f881e8
Paweł Kierski <pkierski@gmail.com>
2024-12-08 13:02:15 +0100

    Remove unused code

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"net"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/hashicorp/go-cleanhttp"
@@ -38,65 +36,4 @@ func main() {
 	dr := trialdownloader.BulkDownload(context.Background(), client.StandardClient(), date, cd)
 
 	trialdownloader.SaveJson(fmt.Sprintf("dr_%v.json", date), dr)
-
-	// courtsData := trialdownloader.DetectBulk(context.Background(), client.StandardClient(), data.Domains)
-	// f1, err := os.Create("courts.json")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer f1.Close()
-	// encoder := json.NewEncoder(f1)
-	// encoder.SetIndent("", "  ")
-	// encoder.Encode(courtsData)
-
-	// b := &bytes.Buffer{}
-	// for _, cd := range courtsData {
-	// 	isV1 := slices.Contains(cd.AppTypes, trialdownloader.AppTypeV1)
-	// 	fmt.Fprintf(b, "%v,%v\n", cd.Domain, isV1)
-	// }
-	// os.WriteFile("courts.txt", b.Bytes(), 0o644)
-
-	return
-
-	// domains, v1Results := bulktest.BulkV1Test(context.Background(), client.StandardClient())
-
-	// f1, err := os.Create("v1_with_false.csv")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer f1.Close()
-	// for i, d := range domains {
-	// 	fmt.Fprintf(f1, "%v,%v\n", d, v1Results[i])
-	// }
-
-	// f2, err := os.Create("v1_with_empty.csv")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer f2.Close()
-	// for i, d := range domains {
-	// 	if !v1Results[i] {
-	// 		d = ""
-	// 	}
-	// 	fmt.Fprintln(f2, d)
-	// 	// fmt.Fprintf(f2, "%v: %v\n", d, v1Results[i])
-	// }
-	// return
-
-	if len(os.Args) < 2 {
-		fmt.Printf("Podaj adres sądu do sprawdzenia wokandy (np.: https://poznan.so.gov.pl)")
-		return
-	}
-
-	// TODO: use constructor based on url
-	downloader := trialdownloader.NewV1Wokanda(client.StandardClient(), os.Args[1])
-
-	trials, err := downloader.Download(context.Background(), "2024-11-27")
-	if err != nil {
-		panic(err)
-	}
-
-	trialdownloader.SortTrials(trials)
-	j, _ := json.MarshalIndent(trials, "", "  ")
-	fmt.Println(string(j))
 }
