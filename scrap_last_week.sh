7dc9288b65d0127320f72c4d792cc503b5102507
Paweł Kierski <pkierski@gmail.com>
2025-01-06 09:50:27 +0100

    Update scrap script to be consistent with Makefile

--- scrap_last_week.sh
+++ scrap_last_week.sh
@@ -3,10 +3,10 @@
 DIR=/root/sandbox
 
 cd $DIR
-$DIR/wokanda-scrapper -insecure-skip-verify
-$DIR/wokanda-scrapper -insecure-skip-verify -relative-scrap-date=-1
-$DIR/wokanda-scrapper -insecure-skip-verify -relative-scrap-date=-2
-$DIR/wokanda-scrapper -insecure-skip-verify -relative-scrap-date=-3
-$DIR/wokanda-scrapper -insecure-skip-verify -relative-scrap-date=-4
-$DIR/wokanda-scrapper -insecure-skip-verify -relative-scrap-date=-5
-$DIR/wokanda-scrapper -insecure-skip-verify -relative-scrap-date=-6
+$DIR/scrapper -insecure-skip-verify
+$DIR/scrapper -insecure-skip-verify -relative-scrap-date=-1
+$DIR/scrapper -insecure-skip-verify -relative-scrap-date=-2
+$DIR/scrapper -insecure-skip-verify -relative-scrap-date=-3
+$DIR/scrapper -insecure-skip-verify -relative-scrap-date=-4
+$DIR/scrapper -insecure-skip-verify -relative-scrap-date=-5
+$DIR/scrapper -insecure-skip-verify -relative-scrap-date=-6
